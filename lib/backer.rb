1d2d1ba240eff0e01beafe6b5fcb4aef656dfaa9
Cheryl K <cherylkelson@icloud.com>
2018-10-06 13:56:58 +0000

    automatically backed up by learn

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+class Backer  
+  
+  attr_accessor :list, :report
+  
+  
+  
+  def initialize
+  @backed_project = [] 
+  
+  end
+  
+   << project
+  
+  
+  
+  
+end 
---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project 
+
+  
+  attr_accessor :list, :report 
+  
+  def initialize 
+    @backers = []
+  
+  end 
+  
+  << backer
+  
+  
+  
+  
+end 
