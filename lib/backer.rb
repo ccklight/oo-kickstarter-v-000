16892cfcee8e587576eaf27347a49b10109eebe2
Cheryl K <cherylkelson@icloud.com>
2018-10-06 14:30:40 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,15 +3,13 @@ class Backer
    
   def initialize(name)
     @name = name
-    @backed_project = [] 
+    @backed_projects = [] 
   end 
     
   def back_project(project) 
     backed_projects << project  
     project.backers << self 
   end 
-  
-  
   
   
 end
